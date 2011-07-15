3886345a560d6050065ee52ab195558d896903e6
Libin Pan <libinpan@gmail.com>
2011-07-15 14:36:12 -0400

    Ruby 1.9 & OS X

--- extconf.rb
+++ extconf.rb
@@ -5,6 +5,8 @@ require "mkmf"
 dir_config("ribusb")
 
 pkg_config("libusb-1.0")
-$CFLAGS += " -Wall"
+$CFLAGS << " -Wall "
+ENV['ARCHFLAGS'] = "-arch i386" if RUBY_PLATFORM =~ /darwin/
+$LIBS << " -lusb-1.0 "
 
 create_makefile("ribusb")
